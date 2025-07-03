76a165fb97cb10135bbb6b40cc353ecf91249eaa
dezhizhang <1541609448@qq.com>
2025-07-04 05:47:28 +0800

    feat: 添加场景状态公有接口

---
+++ Assets/Scripts/Framework/ISceneState.cs
@@ -0,0 +1,25 @@
+public class ISceneState
+{
+    // 场景名称
+    private string _sceneName;
+
+    // 状态的拥有者
+    private SceneStateController _controller;
+
+    public ISceneState(string sceneName)
+    {
+        _sceneName = sceneName;
+    }
+
+    public virtual void StateStart()
+    {
+    }
+
+    public virtual void StateEnd()
+    {
+    }
+
+    public virtual void StateUpdate()
+    {
+    }
+}
